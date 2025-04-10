45877b87ef26e730cabf1a9871e11f080ba4f463
Tpatterson1224 <Tpatterson@guilford.edu>
2025-04-10 11:36:34 -0400

    j

---
+++ testscript.js
@@ -0,0 +1,24 @@
+myitem = document.getElementById("firsttest")
+myitem.addEventListener("click", onClick);
+function onClick() {  
+    myitem.style.color = "green";  // Change text color to green
+    myitem.style.fontSize = "36px";  // Change font size
+}
+myitem.addEventListener("mouseout", onMouseOut);
+function onMouseOut() {
+    myitem.style.color = ""; // Reset the color
+    myitem.style.fontSize = ""; // Reset the font size
+}
+thebutton = document.getElementById("thebutton");
+otheritem = document.getElementById("buttontest");
+thebutton.addEventListener("click", onButtonClick);
+function onButtonClick() {
+    otheritem.style.color = "red";  // Change the text color to red
+}
+textentry = document.getElementById("myinput");
+textentry.addEventListener("change", onChange);
+function onChange() {
+    newtext = textentry.value;  // Get the value from the input field
+    otheritem.innerHTML = newtext;  // Update the content of the other element
+}
+
