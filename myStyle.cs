125250e5863e2e3b4a9902356ab10fb5c502adbe
Tpatterson1224 <Tpatterson@guilford.edu>
2025-04-09 22:09:09 -0400

    nnnn
    njjjj

---
+++ myStyle.cs
@@ -0,0 +1,20 @@
+body {
+    color: blue;
+}
+
+<link rel="stylesheet" type="text/css" href="myStyle.css">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Web Page</title>
+    <link rel="stylesheet" type="text/css" href="myStyle.css">
+</head>
+<body>
+    <h1>Welcome to My Web Page</h1>
+    <p>This is a simple paragraph to demonstrate blue text.</p>
+</body>
+</html>
+
