892ea91bc75b87a407aababb0e5547bed0be5bc4
Tpatterson1224 <Tpatterson@guilford.edu>
2025-04-09 22:22:12 -0400

    ff

--- myStyle.cs
+++ myStyle.cs
@@ -69,3 +69,85 @@ em {
     background-color: lightyellow; /* Background color for em text */
     font-style: italic; /* Italicize the em text */
 }
+
+h1, h2 {
+    color: darkred;
+}
+
+p strong {
+    color: darkkhaki;
+}
+
+<p class="intro">This is the introduction.</p>
+<p class="conclusion">This is the conclusion.</p>
+
+p.intro {
+    font-style: italic;
+}
+
+p.conclusion {
+    font-weight: bold;
+}
+
+<img id="img1100" src="image.jpg" alt="Unique Image">
+
+img#img1100 {
+    width: 50%;
+}
+
+p {
+    width: 80%;
+}
+
+div {
+    padding: 1em;
+}
+
+p {
+    margin-bottom: 1em;
+}
+
+body {
+    margin-left: 2em;
+    margin-right: 2em;
+}
+
+
+<div class="intro">
+    <p>Introduction paragraph 1.</p>
+    <p>Introduction paragraph 2.</p>
+</div>
+
+
+div.intro {
+    padding: 20px;
+    background-color: #f0f0f0;
+}
+
+<header class="flexible">
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+    <a href="#contact">Contact</a>
+</header>
+
+.flexible {
+    display: flex;
+    justify-content: space-around;
+    background-color: #333;
+}
+.flexible a {
+    color: white;
+    text-decoration: none;
+    padding: 10px;
+}
+
+.flexible a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+<footer class="flexible">
+    <p>© 2025 Black goose gym. All Rights Reserved.</p>
+    <p>Last updated: April 2025</p>
+</footer>
+
